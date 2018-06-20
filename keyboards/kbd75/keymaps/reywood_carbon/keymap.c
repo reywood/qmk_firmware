bd23d7b92948d17dc824102e525c928d394fa006
Sean Dwyer <sean.dwyer@gmail.com>
2018-06-20 14:52:42 -0700

    Add more media key functionality to function layer on carbon keyboard

--- keyboards/kbd75/keymaps/reywood_carbon/keymap.c
+++ keyboards/kbd75/keymaps/reywood_carbon/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------------------.
- * | Esc | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 | Ply | VUp | VDn |
+ * | Esc | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 | Ply | VDn | VUp |
  * |-----------------------------------------------------------------------------------------------+
  * |  `  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |   Bkspc   | Hom |
  * |-----------------------------------------------------------------------------------------------+
@@ -23,12 +23,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 	LAYOUT(
-		KC_ESC,  KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,   KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_MPLY, KC_VOLD, KC_VOLU,
-		KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  ______, KC_BSPC, KC_HOME,
-		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_PGUP,
-		KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,          KC_PGDN,
-		KC_LSFT, ______,  KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   KC_UP,   KC_END,
-		KC_LCTL, KC_LALT, KC_LGUI,              KC_SPC, KC_SPC, KC_SPC,             KC_RALT, MO(1),   KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT),
+		KC_ESC,  KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,   KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MPLY, KC_VOLD,   KC_VOLU,
+		KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  ______,  KC_BSPC,   KC_HOME,
+		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,   KC_PGUP,
+		KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,                    KC_ENT,   KC_PGDN,
+		KC_LSFT, ______,  KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,  KC_M,    KC_COMM,    KC_DOT,    KC_SLSH,     KC_RSFT,   KC_UP,    KC_END,
+		KC_LCTL, KC_LALT, KC_LGUI,              KC_SPC, KC_SPC, KC_SPC,               KC_RALT,    MO(1),     KC_RCTL,   KC_LEFT, KC_DOWN,   KC_RGHT),
 
 /* Fn 1
  * ,-----------------------------------------------------------------------------------------------.
@@ -40,9 +40,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------------------------------------------+
  * |         |     |  L  |  D  |  R  |     |     |     |     |     |     |     |             |     |
  * |-----------------------------------------------------------------------------------------------+
- * |           |     | BLT | BL- | BL+ | BLS |     |     |     |     |     |           |     |     |
+ * |           |     | BLT | BL- | BL+ | BLS |     |     |     |     |     |           | VUp |     |
  * |-----------------------------------------------------------------------------------------------+
- * |      |       |       |                                   |     |      |     |     |     |     |
+ * |      |       |       |            Play/Pause             |     |      |     | MPr | VDn | MNx |
  * `-----------------------------------------------------------------------------------------------'
  */
 
@@ -51,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, RESET, ______,
 		______, ______, ______, KC_UP, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, ______, ______, ______,
 		______, ______, KC_LEFT, KC_DOWN, KC_RGHT, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-		______, ______, BL_TOGG, BL_DEC, BL_INC, BL_STEP, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-		______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______),
+		______, ______, BL_TOGG, BL_DEC, BL_INC, BL_STEP, ______, ______, ______, ______, ______, ______, ______, KC_VOLU, ______,
+		______, ______, ______,               ______, KC_MPLY, ______,               ______, ______, ______, KC_MRWD, KC_VOLD, KC_MFFD),
 
 	LAYOUT(
 		______, M(1), M(2), ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
